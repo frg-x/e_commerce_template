30c1e5669d2e7ae9e4fbf73a7ec9a84102f78687
frg-x <frg_x@mail.ru>
2021-06-24 17:51:59 +0300

    added FavoriteScreen

--- lib/screens/checkout/checkout_bottom_nav_bar.dart
+++ lib/screens/checkout/checkout_bottom_nav_bar.dart
@@ -1,9 +1,7 @@
 import 'package:e_commerce_template/constants.dart';
 import 'package:e_commerce_template/cubit/cart/cart_cubit.dart';
 import 'package:e_commerce_template/cubit/delivery/delivery_cubit.dart';
-import 'package:e_commerce_template/cubit/toggle_botnavbar/toggle_botnavbar_cubit.dart';
 import 'package:e_commerce_template/screens/tabs_screen.dart';
-import 'package:e_commerce_template/screens/verify_user_status_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
@@ -90,9 +88,6 @@ class _CheckoutBottomNavBarState extends State<CheckoutBottomNavBar> {
                           width: 239.0,
                           child: TextButton(
                             onPressed: () {
-                              context
-                                  .read<ToggleBotNavBarCubit>()
-                                  .toggleBottomNavBarMenu(0);
                               Navigator.pushReplacementNamed(
                                   context, TabsScreen.routeName);
                               Future.delayed(Duration(seconds: 1)).then((_) {
